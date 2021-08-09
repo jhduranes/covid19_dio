33f69750651b452008b231161c552dd39f5afe1d
Jhonny Duranes <34072575+jhduranes@users.noreply.github.com>
2021-08-09 17:51:03 -0400

    fim aula 7

--- covid19_dio/src/containers/Main/index.jsx
+++ covid19_dio/src/containers/Main/index.jsx
@@ -1,11 +1,13 @@
 import React, { memo, useCallback, useEffect, useState } from 'react'
 import Api from '../../api'
 import Board from './components/Board'
+import Panel from './components/Panel'
 import { ContainerStyled } from './style'
 
 function Main() {
     const [data, setData] = useState({})
     const [country, setCountry] = useState('brazil')
+    const updateAt = new Date().toLocaleString()
 
 
     const getCovidData = useCallback((country) => {
@@ -17,10 +19,21 @@ function Main() {
         getCovidData(country)
     }, [getCovidData, country])
 
+    const handleChange = ({ target }) => {
+        const country = target.value
+        setCountry(country)
+    }
+
     return (
         <ContainerStyled>
             <div className="mb-2">
-
+                <Panel
+                    data={data}
+                    updateAt={updateAt}
+                    onChange={handleChange}
+                    country={country}
+                    getCovidData={getCovidData}
+                />
             </div>
             <Board data={data} />
         </ContainerStyled>
